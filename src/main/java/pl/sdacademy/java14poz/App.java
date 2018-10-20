4f1473aa06358a88136b3ca1f3deb7f0801c00c7
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-10-20 12:05:16 +0200

    Zadanie sumaLiczb

--- src/main/java/pl/sdacademy/java14poz/App.java
+++ src/main/java/pl/sdacademy/java14poz/App.java
@@ -38,22 +38,38 @@ public class App {
         String obiad = coNaObiad();
         System.out.println(obiad);
         System.out.println(poproszeFrytki(20));
+        // wykonanie metody sumaLiczb
+        System.out.println(sumaLiczb(1,2,3));
     }
 
     /**
      * Przyklad metody bez argumentowej
+     *
      * @return co na obiad
      */
-    public static String coNaObiad(){
+    public static String coNaObiad() {
         return "pomidorowa";
     }
 
     /**
      * Przyklad metody z argumentem
+     *
      * @param kwota pieniedzy jakie dajemy
      * @return reszte z tranzakcji
      */
-    public static int poproszeFrytki(int kwota){
-        return kwota-5;
+    public static int poproszeFrytki(int kwota) {
+        return kwota - 5;
+    }
+
+    /**
+     * Zadanie suma 3 liczb
+     *
+     * @param a liczba 1
+     * @param b liczba 2
+     * @param c liczba 3
+     * @return suma trzech liczb
+     */
+    public static int sumaLiczb(int a, int b, int c) {
+        return a + b + c;
     }
 }
