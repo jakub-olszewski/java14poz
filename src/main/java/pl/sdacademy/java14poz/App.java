c8e6eaecd3851eab1807c207187d549548445147
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-10-20 11:20:07 +0200

    Tworzenie zmiennej

--- src/main/java/pl/sdacademy/java14poz/App.java
+++ src/main/java/pl/sdacademy/java14poz/App.java
@@ -19,5 +19,17 @@ public class App {
     public static void main(String[] args) {
         //sout + tab
         System.out.println("Hello World!");
+        System.out.println("Witaj");
+
+        int mojaLiczba;// zmienna z malej litery
+        // jest to deklaracja
+
+        mojaLiczba = 3; // inicjalizacja zmiennej
+        System.out.println(mojaLiczba);
+
+        int mojaDrugaLiczba = 3; // jednoczesnie
+        // deklaracje i inicjalizacje
+
+        System.out.println(Math.PI);
     }
 }
